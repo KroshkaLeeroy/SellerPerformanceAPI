f327bf9ac251833167e2754ed8b5eba58b1487da
KroshkaLeeroy <vasiliuvetrov@mail.ru>
2024-04-23 22:47:46 +0300

    Удаление из истории зависших запросов

--- application/rest_api.py
+++ application/rest_api.py
@@ -6,7 +6,7 @@ from flask_restx import Api, Resource
 from threading import Thread
 from application.request_controller import ControllerRequests
 from application.utils import add_user_query_to_history, check_if_query_history_exists
-from main_structure.new.utils import read_json
+from main_structure.new.utils import read_json, write_json
 from application.config import ADMIN_KEY
 import logging
 
@@ -66,32 +66,6 @@ class DownloadReport(Resource):
             return {'status': 'bad request'}, 400
 
 
-@api.route('/download-stats/<uid>/<date_from>/<date_to>/<file_path>', methods=['GET'])
-class DownloadStat(Resource):
-    def get(self, uid, date_from, date_to, file_path):
-        try:
-            date = f'{date_from}_{date_to}'
-            path = f'downloads/{uid}/{date}/stat/{file_path}'
-            stat_file = read_json(path)
-            return jsonify(stat_file)
-        except Exception as e:
-            print(e)
-            return {'status': 'bad request'}, 400
-
-
-@api.route('/list-stats-folder/<uid>/<date_from>/<date_to>', methods=['GET'])
-class CheckStat(Resource):
-    def get(self, uid, date_from, date_to):
-        try:
-            date = f'{date_from}_{date_to}'
-            path = f'downloads/{uid}/{date}/stat'
-            stat_file = os.listdir(os.path.abspath(path))
-            return jsonify(stat_file)
-        except Exception as e:
-            print(e)
-            return {'status': 'bad request'}, 400
-
-
 @api.route('/<uid>/<path>', methods=['GET'])
 class PathFiles(Resource):
     def get(self, uid, path):
@@ -108,3 +82,19 @@ class PathFiles(Resource):
                 path = os.path.abspath(path)
                 return send_file(path, as_attachment=True)
             return {'status': str(e)}, 400
+
+
+
+@api.route('/<uid>/delete-status/<user_id>/<status_id>', methods=['GET'])
+class DeleteStatus(Resource):
+    def get(self, uid, user_id, status_id):
+        try:
+            if uid == ADMIN_KEY:
+                data = check_if_query_history_exists('history.json')
+                data['users'][user_id]['history'].pop(int(status_id))
+                write_json(data, 'history.json')
+                return {'status': 'ok'}, 200
+            return {'status': 'bad request'}, 400
+        except Exception as e:
+            print(e)
+            return {'status': 'bad request'}, 400
