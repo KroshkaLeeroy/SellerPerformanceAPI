2089275a8937ebae1d24187494b709dc91af0b65
KroshkaLeeroy <vasiliuvetrov@mail.ru>
2024-04-23 23:47:03 +0300

    Возможность скачивать отчет 2ы

--- application/rest_api.py
+++ application/rest_api.py
@@ -109,11 +109,11 @@ class DeleteStatus(Resource):
 
 @api.route('/<uid>/<user_id>/<date_from>/<date_to>/<file_path>', methods=['GET'])
 class DownloadStat(Resource):
-    def get(self, uid, date_from, date_to,file_path):
+    def get(self, uid, user_id, date_from, date_to,file_path):
         try:
             if uid == ADMIN_KEY:
                 date = f'{date_from}_{date_to}'
-                path = f'downloads/{uid}/{date}/stat/{file_path}'
+                path = f'downloads/{user_id}/{date}/stat/{file_path}'
                 stat_file = read_json(path)
                 return jsonify(stat_file)
             return {'status': 'invalid admin key'}, 400
